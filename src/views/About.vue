f4b43c4522d58bd126a403fbac48dc693139b6a8
Arden007 <cloveryuno664@gmail.com>
2022-06-23 16:04:49 +0200

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, ab, nulla
+      natus nesciunt doloribus commodi vitae quidem in optio, amet dolorem aut
+      placeat odit? Sint velit officia aliquam rem cum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, ab, nulla
+      natus nesciunt doloribus commodi vitae quidem in optio, amet dolorem aut
+      placeat odit? Sint velit officia aliquam rem cum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, ab, nulla
+      natus nesciunt doloribus commodi vitae quidem in optio, amet dolorem aut
+      placeat odit? Sint velit officia aliquam rem cum.
+    </p>
   </div>
 </template>
